3186aa32e50c97962c5cc5c9ec9358cd7c6464fc
Alan Barbosa <alanzinhob@hotmail.com>
2024-02-05 09:47:51 -0300

    05_02

--- src/app/add-form.tsx
+++ src/app/add-form.tsx
@@ -12,7 +12,7 @@ function SubmitButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" aria-disabled={pending}>
+    <button type="submit" aria-disabled={pending} className="border px-4">
       Add
     </button>
   );
@@ -22,9 +22,9 @@ export function AddForm() {
   const [state, formAction] = useFormState(createTodo, initialState);
 
   return (
-    <form action={formAction} className="flex flex-col justify-center items-center w-32 m-16">
+    <form action={formAction} className="flex flex-col justify-center items-center w-full my-20 gap-3">
       <label htmlFor="todo">Enter Task</label>
-      <input type="text" id="todo" name="todo" required />
+      <input type="text" id="todo" name="todo" required className="border"/>
       <SubmitButton />
       <p aria-live="polite" className="sr-only" role="status">
         {state?.message}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import {sql} from "@vercel/postgres"
 // import { DeleteForm } from "@/app/delete-form";
 import { AddForm } from "./add-form";
+import { DeleteForm } from "./delete-form";
 
 export default async function Home() {
   let data = await sql`SELECT * FROM todos`;
@@ -10,11 +11,11 @@ export default async function Home() {
     <main className="flex flex-col">
       <h1 className="sr-only">Todos</h1>
       <AddForm />
-      <ul className="flex flex-col">
+      <ul className="flex flex-col justify-center items-center gap-3 w-full">
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li key={todo.id} className="flex">
             {todo.text}
-            {/* <DeleteForm id={todo.id} todo={todo.text} /> */}
+            <DeleteForm id={todo.id} todo={todo.text} />
           </li>
         ))}
       </ul>
